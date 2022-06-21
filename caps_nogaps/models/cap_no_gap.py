494d5e89ef56d87825ba7b5cdda21ab6e3cc1b43
hafiz11339 <hafizumair.ie@gmail.com>
2022-06-21 18:32:19 +0500

    commot CNG

--- caps_nogaps/models/cap_no_gap.py
+++ caps_nogaps/models/cap_no_gap.py
@@ -12,6 +12,8 @@ class CapNoGap(models.Model):
     product_id = fields.Many2one("product.product")
     package_id = fields.Many2one("variant.package")
     name = fields.Char(related='product_id.name')
+    daily_pack = fields.Char()
+    daily_pack_sku = fields.Char()
     pcs_per_day = fields.Float()
     pcs_per_bag = fields.Integer()
     bags_per_carton = fields.Integer()
