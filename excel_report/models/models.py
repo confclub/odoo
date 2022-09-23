7893635896ee0450528ff95dd1e787cf74182984
hafiz11339 <hafizumair.ie@gmail.com>
2022-09-23 12:32:04 +0500

    date

--- excel_report/models/models.py
+++ excel_report/models/models.py
@@ -94,7 +94,7 @@ class ExcelReport(models.Model):
         # json.loads(cirrent_move.invoice_payments_widget)
         # reconsiles = json.loads(cirrent_move.invoice_payments_widget)['content'][0]['account_payment_id']
         all_invoices = self.env['account.move'].search(
-            [('move_type', '=', 'out_invoice'), ('invoice_date', '<=', '30/06/2022'),('invoice_date', '>=', '01/06/2022'),
+            [('move_type', '=', 'out_invoice'), ('invoice_date', '<', '30/06/2022'),('invoice_date', '>=', '01/06/2022'),
              ('payment_state', '=', 'in_payment'), ('state', '=', 'posted')])
 
         # payment_jaurnal = self.env['account.journal'].search([('name','=', 'Old Everyday Account'),('type', '=', 'bank')],limit=1)
